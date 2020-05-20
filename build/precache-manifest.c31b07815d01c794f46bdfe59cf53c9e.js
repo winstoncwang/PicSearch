56d51fefed94a5b43eec809f2d85be3cb8e872e7
winstoncwang <congwang@live.co.uk>
2020-05-20 19:37:20 +0100

    Version 1 Build
    version 1 build

---
+++ build/precache-manifest.c31b07815d01c794f46bdfe59cf53c9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af34a91c4d95cc44f4d9c5de1e1e0f83",
+    "url": "/PicSearch/index.html"
+  },
+  {
+    "revision": "d7e834660989be88fc14",
+    "url": "/PicSearch/static/js/2.f4bb7e63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PicSearch/static/js/2.f4bb7e63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ddf7c765bee688c1b32",
+    "url": "/PicSearch/static/js/main.46b6621f.chunk.js"
+  },
+  {
+    "revision": "0c713ca29f47d5f64544",
+    "url": "/PicSearch/static/js/runtime-main.766ce0df.js"
+  }
+]);
